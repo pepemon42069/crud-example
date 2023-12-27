8bf08ba4b3649e552c91e7ce35c859256270e7dd
pepemon42069 <pepemon42069@proton.me>
2023-12-27 18:47:53 +0530

    init project

---
+++ src/main/java/com/test/doofus/DoofusApplication.java
@@ -0,0 +1,13 @@
+package com.test.doofus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoofusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoofusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/doofus/DoofusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.doofus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoofusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
